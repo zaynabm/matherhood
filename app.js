8dfa3fd802ab20a1ae2f3094768d237b107b9929
zaynab <zaynab_alariny@outlook.com>
2019-02-03 18:40:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,66 @@
-const http = require('http');
+// const http = require('http');
+//
+// const port=process.env.PORT || 3000
+//
+// const server = http.createServer((req, res) => {
+//
+// res.statusCode = 200;
+//
+// res.setHeader('Content-Type', 'text/html');
+//
+// res.end('<h1>Hello World</h1>');
+//
+// });
+//
+// server.listen(port,() => {
+//
+// console.log(`Server running at port `+port);
+//
+// });
 
+
+
+
+var config =require('./config').config
+var log =require('./helperFuncs').log
+var debug =require('./helperFuncs').debug
+//----------------------------------------------------------------------------------------------------------------------------------------------------//
+//------------------------------------------------------------------- APIs----------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------------------------------------------------------------------------//
+// var port = 8090
 const port=process.env.PORT || 3000
+var express = require("express")
+ , bodyParser = require('body-parser');
+var app=express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(bodyParser.json());
+app.use(function (err, req, resp, next) {
+    if(err) {
+        msg = "Bad json object"
+        data ={}
+        statusCode = 400
+        resp.statusCode= statusCode
+        resp.send(config.HttpResp(msg,data))
+    }
+})
 
+
+var loginController=require("./controllers/login");
+var signupController=require("./controllers/signup");
+
+
+app.use("/login",loginController);
+app.use("/signup",signupController);
+
+
+app.listen(port);
+debug("Hello from port "+port+" :D" );
 const server = http.createServer((req, res) => {
-
-res.statusCode = 200;
-
-res.setHeader('Content-Type', 'text/html');
-
-res.end('<h1>Hello World</h1>');
-
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hellllo World</h1>');
 });
 
 server.listen(port,() => {
@@ -18,3 +69,6 @@ console.log(`Server running at port `+port);
 
 });
 
+process.on('unhandledRejection', (reason, p) => {
+  log('Unhandled Rejection at: Promise'+ p+ 'reason:'+ reason);
+});
